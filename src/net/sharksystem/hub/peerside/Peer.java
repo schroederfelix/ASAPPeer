b602f8b095a5e8f226e7fedb6c6004821446c696
Felix Schröder <felix.schroeder@online-bb.de>
2021-11-11 15:02:29 +0100

    added basic Peer

---
+++ src/net/sharksystem/hub/peerside/Peer.java
@@ -0,0 +1,42 @@
+package net.sharksystem.hub.peerside;
+import net.sharksystem.asap.ASAPException;
+
+import java.io.*;
+
+public class Peer
+{
+
+	static final CharSequence ALICE = "Alice";
+	static final CharSequence BOB = "Bob";
+	static final CharSequence YOUR_APP_NAME = "Peer";
+	static final CharSequence YOUR_URI = "yourSchema://example";
+
+	private static final int PORT = 7777;
+
+	private static int port = 0;
+	static int getPortNumber() {
+		if(Peer.port == 0) {
+			Peer.port = PORT;
+		} else {
+			Peer.port++;
+		}
+
+		return Peer.port;
+	}
+
+	public static void main(String[] args) throws IOException, ASAPException {
+
+		HubConnector hubConnector = SharedTCPChannelConnectorPeerSide.createTCPHubConnector("localhost", 6910);
+		//ASAPEncounterManagerImpl em = new ASAPEncounterManagerImpl();
+		//NewConnectionListener listener = new NewConnectionListenerImpl(em);
+		//hubConnector.addListener(listener);
+
+		// tell hub who you are - other peers will find you
+		hubConnector.connectHub(ALICE);
+		// get list of peer connected to this hub
+		hubConnector.getPeerIDs();
+		// get fresh status information from hub, especially peer ids
+		hubConnector.syncHubInformation();
+
+	}
+}
